c36ceccdc7817a805b2938c4f509f3c15ce72feb
Enti-Pol <pol.garriga@enti.cat>
2020-11-27 11:17:42 +0100

    Seguimos con MD5 en MP4

--- md5mp4.sh
+++ md5mp4.sh
@@ -3,5 +3,9 @@
 echo "Ejercicio Bucles y MD5"
 
 for var in `ls *.mp4`; do
-	md5sum $var | cut -d " " -f 1
+	echo "------------------------"
+	VIDEO=`file $var | grep Media`
+	if [ "$VIDEO" != "" ]; then
+		md5sum $var | cut -d " " -f 1
+	fi
 done
