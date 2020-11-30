330116b509dbc9c22a719a100887f57ed3ea7aac
Enti-Pol <pol.garriga@enti.cat>
2020-11-30 11:22:51 +0100

    Clase 30/11

--- md5mp4.sh
+++ md5mp4.sh
@@ -2,6 +2,12 @@
 
 echo "Ejercicio Bucles y MD5"
 
-for var in `ls *.mp4`; do
-	md5sum $var | cut -d " " -f 1
+for ARCHIVO in `ls *.mp4`; do
+	echo "-----------------------"
+	VIDEO=`file $ARCHIVO | grep Media`
+	if [ "$VIDEO" != "" ];then
+		#md5sum $ARCHIVO | cut -d " " -f 1
+		NOMBRE=`echo $ARCHIVO | cut -d "." -f 1`
+		ffmpeg -i $ARCHIVO $NOMBRE.mkv
+	fi
 done
